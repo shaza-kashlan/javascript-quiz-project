17d5acf9cb2bc5e581920ea463213b6119e62111
Piero Schenato <piero.schenato@gmail.com>
2024-02-22 16:51:37 +0100

    done with time Remainig

--- src/index.js
+++ src/index.js
@@ -73,6 +73,20 @@ document.addEventListener("DOMContentLoaded", () => {
   /************  TIMER  ************/
 
   let timer;
+  timer = setInterval(() =>{
+    quiz.timeRemaining--; 
+    console.log(`${quiz.timeRemaining}`)
+    const minutes = Math.floor(quiz.timeRemaining / 60)
+    .toString()
+    .padStart(2, "0");
+  const seconds = (quiz.timeRemaining % 60).toString().padStart(2, "0");
+  const timeRemainingContainer = document.getElementById("timeRemaining");
+  timeRemainingContainer.innerText = `${minutes}:${seconds}`;
+if (quiz.timeRemaining === 0) {
+clearInterval(timer) 
+showResults();
+} 
+  },1000)
 
   /************  EVENT LISTENERS  ************/
 
